a9d1374a49259be2e090a42cbb51b78eab8d7953
Miguel Pari Soto <miguel@idwall.co>
2019-11-24 14:26:28 -0300

    fix: reformat all

--- train_liveness.py
+++ train_liveness.py
@@ -14,23 +14,31 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 from keras.utils import np_utils
 from imutils import paths
+from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import pickle
 import cv2
-import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset")
-ap.add_argument("-m", "--model", type=str, required=True,
-	help="path to trained model")
-ap.add_argument("-l", "--le", type=str, required=True,
-	help="path to label encoder")
-ap.add_argument("-p", "--plot", type=str, default="plot.png",
-	help="path to output loss/accuracy plot")
+ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
+ap.add_argument("-m",
+                "--model",
+                type=str,
+                required=True,
+                help="path to trained model")
+ap.add_argument("-l",
+                "--le",
+                type=str,
+                required=True,
+                help="path to label encoder")
+ap.add_argument("-p",
+                "--plot",
+                type=str,
+                default="plot.png",
+                help="path to output loss/accuracy plot")
 args = vars(ap.parse_args())
 
 # initialize the initial learning rate, batch size, and number of
@@ -43,19 +51,21 @@ EPOCHS = 100
 # the list of data (i.e., images) and class images
 print("[INFO] loading images...")
 imagePaths = list(paths.list_images(args["dataset"]))
+print(imagePaths)
+
 data = []
 labels = []
 
 for imagePath in imagePaths:
-	# extract the class label from the filename, load the image and
-	# resize it to be a fixed 32x32 pixels, ignoring aspect ratio
-	label = imagePath.split(os.path.sep)[-2]
-	image = cv2.imread(imagePath)
-	image = cv2.resize(image, (64, 64))
+    # extract the class label from the filename, load the image and
+    # resize it to be a fixed 32x32 pixels, ignoring aspect ratio
+    label = Path(imagePath).parent.stem
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (64, 64))
 
-	# update the data and labels lists, respectively
-	data.append(image)
-	labels.append(label)
+    # update the data and labels lists, respectively
+    data.append(image)
+    labels.append(label)
 
 # convert the data into a NumPy array, then preprocess it by scaling
 # all pixel intensities to the range [0, 1]
@@ -69,34 +79,44 @@ labels = np_utils.to_categorical(labels, 2)
 
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.25, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(data,
+                                                  labels,
+                                                  test_size=0.25,
+                                                  random_state=42)
 
 # construct the training image generator for data augmentation
-aug = ImageDataGenerator(rotation_range=20, zoom_range=0.15,
-	width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15,
-	horizontal_flip=False, fill_mode="nearest")
+aug = ImageDataGenerator(rotation_range=20,
+                         zoom_range=0.15,
+                         width_shift_range=0.2,
+                         height_shift_range=0.2,
+                         shear_range=0.15,
+                         horizontal_flip=False,
+                         fill_mode="nearest")
 
 # initialize the optimizer and model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model = LivenessNet.build(width=64, height=64, depth=3,
-	classes=len(le.classes_))
+model = LivenessNet.build(width=64,
+                          height=64,
+                          depth=3,
+                          classes=len(le.classes_))
 model.summary()
-model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network for {} epochs...".format(EPOCHS))
 H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
-	validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
-	epochs=EPOCHS)
+                        validation_data=(testX, testY),
+                        steps_per_epoch=len(trainX) // BS,
+                        epochs=EPOCHS)
 
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=BS)
-print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=le.classes_))
+print(
+    classification_report(testY.argmax(axis=1),
+                          predictions.argmax(axis=1),
+                          target_names=le.classes_))
 
 # save the network to disk
 print("[INFO] serializing network to '{}'...".format(args["model"]))
